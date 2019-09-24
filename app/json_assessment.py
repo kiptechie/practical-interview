829a1909bd79c3cc788c5a1c8efc0eb940ba5340
salma-nyagaka <salmanyaga@gmail.com>
2019-09-24 17:49:07 +0300

    feat(database): load contents to the database
    - create a connection the to the database
    - loop through the contents and load them to the database

--- app/json_assessment.py
+++ app/json_assessment.py
@@ -204,7 +204,74 @@ clients = '''
   }
 '''
 
-data = json.loads(clients)
+# data = json.loads(clients)
 
-for client in data['Report']:
-  client
+# for client in data['Report']:
+#   client
+
+
+class DatabaseConnection:
+    def __init__(self):
+        try:
+
+            self.connection = psycopg2.connect(dbname='practicalinterview',
+                                               user='postgres',
+                                               host='localhost',
+                                               password='')
+
+            self.cursor = self.connection.cursor()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("\n\n\Error!!! : {}\n\n\n".format(error))
+
+
+    def drop_table(self):
+      drop_table_command= '''DROP TABLE IF EXISTS clients'''
+      self.cursor.execute(drop_table_command)
+      self.connection.commit()
+
+    def create_table(self):
+        create_table_command = '''CREATE TABLE IF NOT EXISTS clients(
+                                    ticketID serial PRIMARY KEY,
+                                    clientName VARCHAR (200) NOT NULL,
+                                    mobileNo VARCHAR (200) NOT NULL,
+                                    contactType VARCHAR (200) NOT NULL,
+                                    callType VARCHAR (200) NOT NULL,
+                                    sourceName VARCHAR (200) NOT NULL,
+                                    storeName VARCHAR (200) NOT NULL,
+                                    questionType VARCHAR (200) NOT NULL,
+                                    questionSubType VARCHAR (200) NOT NULL,
+                                    dispositionName VARCHAR (200) NOT NULL,
+                                    dateCreated VARCHAR (200) NOT NULL
+                                    )'''
+        self.cursor.execute(create_table_command)
+        self.connection.commit()
+
+    def insert_new_method(self):
+      data = json.loads(clients)
+
+      for client in data['Report']:
+          new_record = (client['ticketID'], client['clientName'], client['mobileNo'],
+                        client['contactType'], client['callType'], client['sourceName'],
+                        client['storeName'], client['questionType'], client['questionSubType'],
+                        client['dispositionName'], client['dateCreated'])
+          print(new_record)
+
+          insert_command = "INSERT INTO clients(ticketID, clientName, mobileNo, contactType, callType, sourceName, storeName, questionType, questionSubType, dispositionName, dateCreated) VALUES('" + \
+              new_record[0] + "', '" + new_record[1] + "', '" + \
+              new_record[2] + "', '" + new_record[3] + "','" + \
+              new_record[4] + "', '"+new_record[5] + "', '"+ \
+              new_record[6] + "', '"+new_record[7] + "', '"+ \
+              new_record[8] + "', '"+new_record[9] + "', '"+ \
+              new_record[10] + "')"
+
+
+          self.cursor.execute(insert_command)
+          self.connection.commit()
+
+
+if __name__ == '__main__':
+    database_connection = DatabaseConnection()
+    database_connection.drop_table()
+    database_connection.create_table()
+    database_connection.insert_new_method()
